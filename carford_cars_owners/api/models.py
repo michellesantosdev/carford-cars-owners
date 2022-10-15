55d2b74f989be110d56bf2b36a465b8eb47bb1a3
Michelle Santos <michelle.rigelo@gmail.com>
2022-10-15 16:01:46 -0300

    add docker to up postgres and add models structure

--- carford_cars_owners/api/models.py
+++ carford_cars_owners/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Owner(models.Model):
+    name = models.CharField('NAME', max_length=40, null=True)
+    sale_opportunity = models.BooleanField('Sale Opportunity', default=True)
+
+
+class Car(models.Model):
+    COLOR_CHOICES = (
+        ('YELLOW', 'YELLOW'),
+        ('BLUE', 'BLUE'),
+        ('GRAY', 'GRAY'),
+    )
+
+    MODEL_CHOICES = (
+        ('HATCH', 'HATCH'),
+        ('SEDAN', 'SEDAN'),
+        ('CONVERTIBLE', 'CONVERTIBLE'),
+    )
+
+    name = models.CharField('NAME', max_length=40, null=True)
+    color = models.CharField('COLOR', max_length=15, choices=COLOR_CHOICES, null=False)
+    model = models.CharField('MODEL', max_length=15, choices=MODEL_CHOICES, null=False)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, null=False)
--- carford_cars_owners/settings.py
+++ carford_cars_owners/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'carford_cars_owners.api',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +77,11 @@ WSGI_APPLICATION = 'carford_cars_owners.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+parser_database = dj_database_url.parse(config('DATABASE_URL', default=default_db_url), conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': parser_database
 }
 
 
